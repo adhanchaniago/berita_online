566358a250ebb433d5a298e15af6970764e695bf
uswtunkh <khasanahnganjuk1122@gmail.com>
2020-07-31 16:56:05 -0700

    sprint 4

--- berita_online.sql
+++ berita_online.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 17, 2020 at 11:37 PM
+-- Generation Time: Aug 01, 2020 at 01:54 AM
 -- Server version: 10.4.6-MariaDB
 -- PHP Version: 7.3.9
 
@@ -42,14 +42,12 @@ CREATE TABLE `berita` (
 --
 
 INSERT INTO `berita` (`id`, `idKategori`, `idLogin`, `judul_berita`, `isi_berita`, `jml_baca`) VALUES
-(39, 7, 2, 'Berita Singkat Tentang Banjir', 'Peristiwa banjir bandang akhirnya terjadi lagi. Kali ini menimpa beberapa desa di daerah sekitar kali Brantas. Banjir bandang tersebut diduga disebabkan karena intensitas hujan yang turun deras mulai jam 20.00 WIB hingga 06.00 WIB. Setidaknya sudah 2 desa yang terendam akibat kejadian ini.\r\n\r\nDiantara warga korban yang rumahnya tergenang air, sudah mulai mengungsi ke kampung sebelah. Diperkirakan karena kejadian ini ada sekitar 165 kepala keluarga terpaksa kehilangan tempat tinggalnya. Pemerintah juga sudah tanggap dengan memberikan bantuan yang diperlukan. Seperti air bersih, makanan, minuman, pakaian, serta obat-obatan.\r\n\r\n\r\nMenurut catatan, sebelumnya juga pernah terjadi banjir di wilayah ini tepatnya sekitar 3 tahun yang lalu. Namun diperkirakan, banjir bandang kali ini jauh lebih besar daripada tahun sebelumnya. Dugaan sementara, kejadian ini akibat dari masyarakat kali Brantas sendiri yang membuang sampah sembarangan di sekitaran kali Brantas.', 71),
-(40, 7, 2, 'Berita Singkat Tentang Kecelakaan', 'Telah terjadi peristiwa kecelakaan sekitar dini hari tadi tanggal 5 mei 2015 tepatnya di Jalan Tol Cipali. Kecelakaan ini melibatkan sebuah mobil mewah yang dikemudikan oleh seorang anak di bawah umur. Pengendara ditengarai bernama Bagus. Dirinya diduga mengemudikan mobil dengan kecepatan diatas 130 KM/jam.\r\n\r\nMobil bernomor polisi B 5643 itu terlihat oleh cctv menabrak pembatas jalan. Kejadian ini diduga karena rem mobil blong. Untungnya, mobil Ferrari hitam itu tidak sampai membuat kecelakaan yang hebat. Hal tersebut karena saat kejadian tol dalam keadaan sepi dan kebetulan sudah dini hari.\r\n\r\nKecelakaan yang bisa dikategorikan tunggal  tersebut juga tidak sampai merenggut korban jiwa. Hanya saja, bagus si pengendara mobil terlihat mengalami luka berat. Hingga saat ini dirinya masih dirawat intensif di salah satu rumah sakit terdekat.\r\n\r\n3. Contoh Teks Berita Singkat Tentang Kebakaran\r\nContoh Teks Berita\r\nKejadian kebakaran juga sering menjadi topic beberapa teks berita. Seperti contoh teks berita di bawah ini yang membahas tentang kejadian kebakaran di sebuah rumah. Diduga kejadian tersebut akibat meletakkan lilin yang kurang berhati-hati. Simak contoh paragraf berita ini dengan judul “Diduga karena lilin, rumah habis dilalap si jago merah”.\r\n\r\nKejadian tak terduga terjadi di salah satu perumahan Griya Suci Permai di Gresik kemarin malam sekitar pukul 21.00 WIB. Rumah milik keluarga Pak Agus tersebut kini habis tak bersisa akibat kebakaran. Diduga peristiwa itu terjadi akibat salah satu penghuni rumah yang kurang berhati-hati meletakkan lilin.', 36),
+(39, 7, 2, 'Berita Singkat Tentang Banjir', 'Peristiwa banjir bandang akhirnya terjadi lagi. Kali ini menimpa beberapa desa di daerah sekitar kali Brantas. Banjir bandang tersebut diduga disebabkan karena intensitas hujan yang turun deras mulai jam 20.00 WIB hingga 06.00 WIB. Setidaknya sudah 2 desa yang terendam akibat kejadian ini.\r\n\r\nDiantara warga korban yang rumahnya tergenang air, sudah mulai mengungsi ke kampung sebelah. Diperkirakan karena kejadian ini ada sekitar 165 kepala keluarga terpaksa kehilangan tempat tinggalnya. Pemerintah juga sudah tanggap dengan memberikan bantuan yang diperlukan. Seperti air bersih, makanan, minuman, pakaian, serta obat-obatan.\r\n\r\n\r\nMenurut catatan, sebelumnya juga pernah terjadi banjir di wilayah ini tepatnya sekitar 3 tahun yang lalu. Namun diperkirakan, banjir bandang kali ini jauh lebih besar daripada tahun sebelumnya. Dugaan sementara, kejadian ini akibat dari masyarakat kali Brantas sendiri yang membuang sampah sembarangan di sekitaran kali Brantas.', 35),
+(40, 7, 2, 'Berita Singkat Tentang Kecelakaan', 'Telah terjadi peristiwa kecelakaan sekitar dini hari tadi tanggal 5 mei 2015 tepatnya di Jalan Tol Cipali. Kecelakaan ini melibatkan sebuah mobil mewah yang dikemudikan oleh seorang anak di bawah umur. Pengendara ditengarai bernama Bagus. Dirinya diduga mengemudikan mobil dengan kecepatan diatas 130 KM/jam.\r\n\r\nMobil bernomor polisi B 5643 itu terlihat oleh cctv menabrak pembatas jalan. Kejadian ini diduga karena rem mobil blong. Untungnya, mobil Ferrari hitam itu tidak sampai membuat kecelakaan yang hebat. Hal tersebut karena saat kejadian tol dalam keadaan sepi dan kebetulan sudah dini hari.\r\n\r\nKecelakaan yang bisa dikategorikan tunggal  tersebut juga tidak sampai merenggut korban jiwa. Hanya saja, bagus si pengendara mobil terlihat mengalami luka berat. Hingga saat ini dirinya masih dirawat intensif di salah satu rumah sakit terdekat.\r\n\r\n3. Contoh Teks Berita Singkat Tentang Kebakaran\r\nContoh Teks Berita\r\nKejadian kebakaran juga sering menjadi topic beberapa teks berita. Seperti contoh teks berita di bawah ini yang membahas tentang kejadian kebakaran di sebuah rumah. Diduga kejadian tersebut akibat meletakkan lilin yang kurang berhati-hati. Simak contoh paragraf berita ini dengan judul “Diduga karena lilin, rumah habis dilalap si jago merah”.\r\n\r\nKejadian tak terduga terjadi di salah satu perumahan Griya Suci Permai di Gresik kemarin malam sekitar pukul 21.00 WIB. Rumah milik keluarga Pak Agus tersebut kini habis tak bersisa akibat kebakaran. Diduga peristiwa itu terjadi akibat salah satu penghuni rumah yang kurang berhati-hati meletakkan lilin.', 40),
 (41, 7, 2, ' Berita Singkat Tentang Gempa Bumi', 'Sejumlah peristiwa alam seperti gempa bumi juga bisa dijadikan topik menulis teks berita. Seperti contoh di bawah ini tentang kejadian gempa bumi yang mengguncang daerah Lombok. Berikut teks berita dengan judul “Gunjangan Gempa Lombok, Mengundang Kepanikan Warga”.\r\n\r\nLagi-lagi, bencana kembali menerpa bumi pertiwi. Kali ini gempa bumi mengguncang tanah Lombok, Nusa Tenggara Timur. Gempa berkekuatan 7 skala ritcher mampu membuat kepanikan warga. Sebagian warga Lombok langsung mengungsi ke dataran tinggi sesaat setelah pihak BMKG memberi peringatan ada potensi tsunami.\r\n\r\nGempa 7 skala ritcher ini terjadi sekitar pukul 18.00 waktu Indonesia Tengah. Pusat gempa diperkirakan berada di jarak  20 km barat laut Lombok timur. Kedalaman gempa terukur hingga 15 kilometer. Kepanikan warga sempat bertambah saat berita potensi tsunami diturunkan serta listrik yang mati total.\r\n\r\n  Contoh Teks Laporan Percobaan\r\nDengan segala kegentingan yang terjadi mereka memutuskan mengungsi ke daerah yang lebih tinggi. Namun akhirnya, demi meredam kepanikan warga BMKG mencabut peringatan potensi tsunami. Gempa yang berkekuatan cukup tinggi itu pun berakibat pada naiknya air laut di beberapa daerah.\r\n\r\nDiantaranya daerah Bada Sumbawa serta pelabuhan Carik Lombok utara. Sementara hingga berita ini diturunkan, warga masih merasakan beberapa kali gempa susulan.', 1),
-(44, 4, 4, 'uswa', 'njmnm', 50),
-(45, 4, 6, 'kenken', 'kenken', 0),
-(46, 7, 9, 'rizal', 'rizal', 0),
-(55, 5, 2, 'Teks Berita Singkat Tentang Banjir', 'mnm', 0),
-(57, 5, 2, 'ok', 'ok', 0);
+(61, 5, 13, 'lallalal', 'lalkkla', 0),
+(63, 5, 13, 'Berita ', 'kjxjbhbahjbajh', 0),
+(64, 5, 13, 'pk', 'Peristiwa banjir bandang akhirnya terjadi lagi. Kali ini menimpa beberapa desa di daerah sekitar kali Brantas. Banjir bandang tersebut diduga disebabkan karena intensitas hujan yang turun deras mulai jam 20.00 WIB hingga 06.00 WIB. Setidaknya sudah 2 desa yang terendam akibat kejadian ini.\r\n\r\nDiantara warga korban yang rumahnya tergenang air, sudah mulai mengungsi ke kampung sebelah. Diperkirakan karena kejadian ini ada sekitar 165 kepala keluarga terpaksa kehilangan tempat tinggalnya. Pemerintah juga sudah tanggap dengan memberikan bantuan yang diperlukan. Seperti air bersih, makanan, minuman, pakaian, serta obat-obatan.\r\n\r\n\r\nMenurut catatan, sebelumnya juga pernah terjadi banjir di wilayah ini tepatnya sekitar 3 tahun yang lalu. Namun diperkirakan, banjir bandang kali ini jauh lebih besar daripada tahun sebelumnya. Dugaan sementara, kejadian ini akibat dari masyarakat kali Brantas sendiri yang membuang sampah sembarangan di sekitaran kali Brantas.', 0);
 
 -- --------------------------------------------------------
 
@@ -81,21 +79,24 @@ CREATE TABLE `login` (
   `idLogin` int(50) NOT NULL,
   `nama` varchar(64) NOT NULL,
   `username` varchar(50) DEFAULT NULL,
-  `email` varchar(128) NOT NULL,
   `password` varchar(32) DEFAULT NULL,
-  `confirm_password` varchar(30) NOT NULL,
-  `level` enum('Admin','User') DEFAULT NULL
+  `level` enum('SuperAdmin','Admin') DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `login`
 --
 
-INSERT INTO `login` (`idLogin`, `nama`, `username`, `email`, `password`, `confirm_password`, `level`) VALUES
-(2, 'uswatun', 'wawa', 'khasanah@ymail.com', '81dc9bdb52d04dc20036dbd8313ed055', '', 'Admin'),
-(4, 'USWATUN KHASANAH', 'uswa', 'khasanah', '202cb962ac59075b964b07152d234b70', '123', 'Admin'),
-(6, 'Kenzi Arsya', 'kenzi', 'kenken', '9996535e07258a7bbfd8b132435c5962', '1235', 'Admin'),
-(9, 'rizal', 'rizal', 'khasanahnganjuk1122@gmail.com', '202cb962ac59075b964b07152d234b70', '123', NULL);
+INSERT INTO `login` (`idLogin`, `nama`, `username`, `password`, `level`) VALUES
+(2, 'uswatun', 'wawa', '81dc9bdb52d04dc20036dbd8313ed055', 'Admin'),
+(13, 'Uswatun Khasanah', 'ela', '698d51a19d8a121ce581499d7b701668', 'SuperAdmin'),
+(14, 'Uswatun Khasanah', 'uswa', 'b59c67bf196a4758191e42f76670ceba', 'Admin'),
+(15, 'Uswatun Khasanah', 'uus', '698d51a19d8a121ce581499d7b701668', 'Admin'),
+(16, 'Uswatun Khasanah', 'uswatun', '698d51a19d8a121ce581499d7b701668', 'SuperAdmin'),
+(17, 'Uswatun Khasanah', 'hani', 'bcbe3365e6ac95ea2c0343a2395834dd', 'Admin'),
+(18, 'Uswatun Khasanah', 'hani', 'bcbe3365e6ac95ea2c0343a2395834dd', 'Admin'),
+(21, 'ratna', 'ratna', 'b59c67bf196a4758191e42f76670ceba', 'SuperAdmin'),
+(22, 'ratna', 'ratna', '202cb962ac59075b964b07152d234b70', 'Admin');
 
 --
 -- Indexes for dumped tables
@@ -129,7 +130,7 @@ ALTER TABLE `login`
 -- AUTO_INCREMENT for table `berita`
 --
 ALTER TABLE `berita`
-  MODIFY `id` int(50) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
+  MODIFY `id` int(50) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 
 --
 -- AUTO_INCREMENT for table `kategori`
@@ -141,7 +142,7 @@ ALTER TABLE `kategori`
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `idLogin` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `idLogin` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- Constraints for dumped tables
